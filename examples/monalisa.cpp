70cd95cac122920ab3aa4cae29896eb27868b14c
William Conner <redwils5@gmail.com>
2026-01-04 19:18:32 -0500

    Created Renderer class to cache and contain the sf::RenderTexture used for approximations to be rendered upon

--- examples/monalisa.cpp
+++ examples/monalisa.cpp
@@ -30,50 +30,76 @@ using Approximation = std::array<Triangle, NUM_TRIANGLES>;
 
 const sf::Image monalisa {monalisa_jpg, monalisa_jpg_len};
 
-sf::Vector2f vectorToImageVector(uint16_t x, uint16_t y)
-{
-    static const float max_value = static_cast<float>(std::numeric_limits<uint16_t>::max());
-    static const float img_width = monalisa.getSize().x;
-    static const float img_height = monalisa.getSize().y;
-    return {
-        (static_cast<float>(x) / max_value) * img_width,
-        (static_cast<float>(y) / max_value) * img_height
-    };
-}
+class Renderer {
+    private:
+    sf::Image img_;
+    sf::RenderTexture texture_;
 
-sf::Texture renderApproximation(const Approximation& approx)
-{
-    sf::RenderTexture texture (monalisa.getSize());
-    sf::VertexArray va (sf::PrimitiveType::Triangles);
-    for (const auto& triangle : approx)
+    sf::Vector2f pointToVector(uint16_t x, uint16_t y)
     {
-        sf::Vertex v1, v2, v3;
-        v1.color = triangle.color;
-        v2.color = triangle.color;
-        v3.color = triangle.color;
-        v1.position = vectorToImageVector(triangle.x1, triangle.y1);
-        v2.position = vectorToImageVector(triangle.x2, triangle.y2);
-        v3.position = vectorToImageVector(triangle.x3, triangle.y3);
-        va.append(std::move(v1));
-        va.append(std::move(v2));
-        va.append(std::move(v3));
+        static const float max_value = static_cast<float>(std::numeric_limits<uint16_t>::max());
+        return {
+            (static_cast<float>(x) / max_value) * img_.getSize().x,
+            (static_cast<float>(y) / max_value) * img_.getSize().y
+        };
     }
-    texture.clear();
-    texture.draw(va);
-    texture.display();
 
-    return texture.getTexture();
-} 
+    public:
+    Renderer(sf::Image img)
+    : img_(img)
+    , texture_(img.getSize())
+    { }
+    Renderer(sf::Image img, float scale)
+    {
+        sf::Texture img_tex (img);
+        sf::Sprite sprite (img_tex);
+        sprite.setScale({scale, scale});
+        sf::RenderTexture temp;
+        temp.clear();
+        temp.draw(sprite);
+        temp.display();
+        img_ = temp.getTexture().copyToImage();
+        (void)texture_.resize(img_.getSize());
+    }
+    const sf::Image& getImage()
+    {
+        return img_;
+    }
+    const sf::Texture& renderApproximation(const Approximation& approx)
+    {
+        sf::VertexArray va (sf::PrimitiveType::Triangles);
+        for (const auto& triangle : approx)
+        {
+            sf::Vertex v1, v2, v3;
+            v1.color = triangle.color;
+            v2.color = triangle.color;
+            v3.color = triangle.color;
+            v1.position = pointToVector(triangle.x1, triangle.y1);
+            v2.position = pointToVector(triangle.x2, triangle.y2);
+            v3.position = pointToVector(triangle.x3, triangle.y3);
+            va.append(std::move(v1));
+            va.append(std::move(v2));
+            va.append(std::move(v3));
+        }
+        texture_.clear();
+        texture_.draw(va);
+        texture_.display();
+
+        return texture_.getTexture();
+    }
+};
 
 class Scenario : public genetic::Scenario<Approximation>
 {
     private:
-    inline static const std::string name = "monalisa";
+    inline static const std::string name = "approx";
+    Renderer renderer_;
     genetic::Serializer<Approximation> serializer_;
 
     public: 
-    Scenario()
-    : serializer_(name)
+    Scenario(sf::Image image)
+    : renderer_(image)
+    , serializer_(name)
     { }
     const std::string& getName()
     {
@@ -89,7 +115,7 @@ class Scenario : public genetic::Scenario<Approximation>
         static const std::size_t NUM_PIXELS = monalisa.getSize().x * monalisa.getSize().y * 4;
         
         int sum = 0;
-        sf::Image render = renderApproximation(approx).copyToImage();
+        sf::Image render = renderer_.renderApproximation(approx).copyToImage();
         for (int i = 0; i < NUM_PIXELS; i += 4)
         {
             for (int c = 0; c < 3; ++c) // R, G, & B only
@@ -145,6 +171,7 @@ class View : public genetic::GraphicView<Approximation>
 {
     private:
     static constexpr float ANIMATION_DURATION = 10.f;
+    Renderer renderer_ {monalisa};
 
     void render(sf::RenderWindow& target)
     {
@@ -155,7 +182,7 @@ class View : public genetic::GraphicView<Approximation>
 
         int i = static_cast<int>((members_.size()-1) * percent_completed);
         Approximation approx = members_[i].value;
-        sf::Texture tex = renderApproximation(approx);
+        sf::Texture tex = renderer_.renderApproximation(approx);
         // Render
         target.clear();
         target.draw(sf::Sprite(tex));
@@ -164,7 +191,7 @@ class View : public genetic::GraphicView<Approximation>
 
     public:
     View(): genetic::GraphicView<Approximation>(
-        "Image Approximation", monalisa.getSize()
+        "Image Approximation", renderer_.getImage().getSize()
     ) {}
 };
 
@@ -175,7 +202,7 @@ int main()
     auto cli = genetic::Controller<img::Approximation>
     (
         genetic::GeneticAlgorithm<img::Approximation>(
-            std::make_unique<img::Scenario>(),
+            std::make_unique<img::Scenario>(img::monalisa),
             genetic::selection::rankBased<img::Approximation>,
             10,
             .1f
